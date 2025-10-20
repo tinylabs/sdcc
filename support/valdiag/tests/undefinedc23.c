65745299c3f3686c8baab4bdae88c2af7389b938
Philipp Klaus Krause <pkk@spth.de>
2025-10-20 16:38:08 +0000

    Test cases for N3532, N3340 resolutions of C23 UB, and one resolutions from N3244.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/undefined/sdcc@15740 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/valdiag/tests/undefinedc23.c
+++ support/valdiag/tests/undefinedc23.c
@@ -3,6 +3,26 @@
 // The numbering of the test cases below corresponds to the numbering therein.
 // In many cases, the resolutionw as to make it a constraint violation.
 
+#ifdef TEST38a
+struct A {
+int a;
+int x : (sizeof((struct A*)0)->a * 8); /* ERROR */
+};
+#endif
+
+#ifdef TEST40
+struct f { struct f *x; } *p;
+void g(void)
+{
+(struct f)p; /* ERROR */
+}
+#endif
+
+#ifdef TEST63
+typedef int f(void);
+const f g; /* ERROR */
+#endif
+
 #ifdef TEST87
 struct foo;
 static struct foo x; /* ERROR */
