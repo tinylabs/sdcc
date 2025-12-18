ed19d9c7fe460d54395cf1973959a6c128a1d8e2
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2025-12-18 08:59:35 +0000

    [ucsim-tlcs870] Implement several ld insts
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/trunk/sdcc@15988 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sim/ucsim/src/sims/tlcs.src/alias870c.h
+++ sim/ucsim/src/sims/tlcs.src/alias870c.h
@@ -4,6 +4,16 @@
 #define SET_CF 			instruction_05
 #define CPL_CF 			instruction_06
 
+#define LDW_mx_mn               instruction_08
+#define LDW_mhl_mn              instruction_09
+#define LD_mx_n                 instruction_0a
+#define LD_mhl_n                instruction_0b
+#define LD_A_mx                 instruction_0c
+#define LD_A_mhl                instruction_0d
+#define LD_mx_A                 instruction_0e
+#define LD_mhl_A                instruction_0f
+
+
 // 0 10 - 0 1f
 #define LD_A_rA                 instruction_10
 #define LD_A_rW                 instruction_11
@@ -31,4 +41,25 @@
 #define LD_rD_A                 instruction_45
 #define LD_rL_A                 instruction_46
 #define LD_rH_A                 instruction_47
+#define LD_rrWA_mn              instruction_48
+#define LD_rrBC_mn              instruction_49
+#define LD_rrDE_mn              instruction_4a
+#define LD_rrHL_mn              instruction_4b
+#define LD_rrIX_mn              instruction_4c
+#define LD_rrIY_mn              instruction_4d
+#define LD_rrSP_mn              instruction_4e
 
+// 0 e0 - 0 ef
+
+// 0 f0 - 0 ff
+#define LD_RBS                  instruction_f9
+
+// 1 40 - 1 4f
+#define LD_rA_g                 instruction_140
+#define LD_rW_g                 instruction_141
+#define LD_rC_g                 instruction_142
+#define LD_rB_g                 instruction_143
+#define LD_rE_g                 instruction_144
+#define LD_rD_g                 instruction_145
+#define LD_rL_g                 instruction_146
+#define LD_rH_g                 instruction_147
